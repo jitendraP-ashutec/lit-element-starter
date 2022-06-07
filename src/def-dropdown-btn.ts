19e59cbf528b0c92861561b685a05403690eb4c0
jitendra <jitendra@ashutec.com>
2022-06-07 19:42:54 +0530

    open dropdown on left-bottom right-bottom, and close button on click outside,

--- src/def-dropdown-btn.ts
+++ src/def-dropdown-btn.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import { LitElement, html, css } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
 /**
  * An example element.
@@ -118,69 +118,203 @@ export class DefDropDownBtn extends LitElement {
       display:contents;
     }
   
-
   `;
 
 
   /**
    * The number of times the button has been clicked.
    */
-   @property({type: String})
-   label: string = '';
+  @property({ type: String })
+  label: string = '';
 
-   @property({type: String})
-   class: string = '';
+  @property({ type: String })
+  class: string = '';
 
 
-   @property({type: Boolean})
-   disabled: boolean = false;
+  @property({ type: Boolean })
+  disabled: boolean = false;
 
-   _handleClick(e:Event) {
-    // console.log(e);
-    if(this.shadowRoot){
-      const slot = this.shadowRoot.querySelector('slot');
-      if(slot){
-        let slt:any = slot.assignedNodes();
-        if (slt.length===0){
-          console.log('No content is available')
-        } else {
-          slt.forEach((element:any) => {
-              console.log(element.className)
-              if(element?.classList?.contains('dropdown-menu')){
-                if(element.classList.contains('show')) {
-                  element.classList.remove('show')
-                } else{
-                  element.classList.add('show')
-                }
-              }    
+  @property({ type: String })
+  dropdownPosition: string = 'bottom-left';
+
+
+  private _handleClick(e: any) {
+    let slt: any = this.assignedNodes;
+    if (this.shadowRoot) {
+      const buttons = this.shadowRoot.querySelector('button');
+      if (buttons) {
+        const domRect: DOMRect = buttons.getBoundingClientRect();
+        if (slt.length > 0) {
+          slt.forEach((element: any) => {
+            if (element?.classList?.contains('dropdown-menu')) {
+              if (element.classList.contains('show')) {
+                element.classList.remove('show')
+              } else {
+                // if(this.)
+                const dropdownMenuItem =  element.getBoundingClientRect();
+                const point:any =  this.getPositionPoint(dropdownMenuItem);
+
+                // const positionX = domRect.x + domRect.width;
+                // const positionY = domRect.y + domRect.height;
+                element.classList.add('show')
+                element.style.position = 'absolute';
+                element.style.inset = '0px auto auto 0px';
+                element.style.transform = `translateX(${point.positionX}px) translateY(${point.positionY}px)`;
+                //         position: absolute;
+                // inset: 0px auto auto 0px;
+                // transform: translateX(0px) translateY(2px);
+              }
+            }
           });
         }
-      } else {
-        console.log('in else')
       }
     }
-  
+  }
+
+  getPositionPoint(dropdownMenuItem:DOMRect) {
+    console.log(this.dropdownPosition)
+
+    if (this.shadowRoot) {
+      const button = this.shadowRoot.querySelector('button');
+      console.log(document.documentElement.scrollTop);
+      const scrollX = document.documentElement.scrollTop;
+      // const dropdownMenu = this.shadowRoot.querySelector('.dropdown-menu');
+      // console.log(dropdownMenu);
+      if (button) {
+        
+        const domRect: DOMRect = button.getBoundingClientRect();
+        console.log(domRect);
+        // console.log(dropdownMenuItem.width -  domRect.right)
+// console.log({domRect})
+        switch (this.dropdownPosition) {
+          case 'bottom-left':
+          return {
+              positionX: domRect.x,
+              positionY: (scrollX + domRect.y + domRect.height)
+          }
+            break;
+          case 'bottom-right':
+             let positionX = domRect.right - dropdownMenuItem.width;
+             console.log(positionX);
+             if(positionX <= 0){
+              positionX = domRect.x;
+             }
+            return {
+              positionX: (positionX),
+              positionY: (scrollX + domRect.y + domRect.height)
+          }
+            break;
+
+          case 'top-left':
+            return {
+              positionX: domRect.x ,
+              positionY: (scrollX + domRect.y - dropdownMenuItem.height)
+            }
+
+            break;
+
+          case 'top-right':
+            let positionTopX = domRect.right - dropdownMenuItem.width;
+            console.log(positionTopX);
+            if(positionTopX <= 0){
+              positionTopX = domRect.x;
+            }
+           return {
+             positionX: (positionTopX),
+             positionY: (scrollX + domRect.y - dropdownMenuItem.height)
+         }
+            return {
+              positionX: (positionX),
+              positionY: (scrollX + domRect.y + domRect.height)
+          }
+            break;
+
+          case 'left-bottom':
+
+            break;
+
+          case 'left-top':
+
+            break;
+
+          case 'right-top':
+            return {
+              positionX: domRect.x + domRect.width,
+              positionY: domRect.y
+            }
+            break;
+
+          case 'right-bottom':
+            return {
+              positionX: domRect.x + domRect.width,
+              positionY: domRect.y
+            }
+
+            break;
+
+
+
+          default:
+            break;
+        }
+
+      }
+    }
+    // dropdownPosition
+  }
+  get assignedNodes() {
+    if (this.shadowRoot) {
+
+      const slot = this.shadowRoot.querySelector('slot');
+
+      if (slot) {
+        let slt: any = slot.assignedNodes();
+        console.log(slt);
+        return slt;
+      }
+    }
+    return [];
+  }
+
+  _closeButton = () => {
+    let slt: any = this.assignedNodes;
+    if (slt && slt.length > 0) {
+      slt.forEach((element: any) => {
+        if (element?.classList?.contains('dropdown-menu')) {
+          element.classList.remove('show')
+        }
+      });
+    }
   }
 
   override connectedCallback() {
     super.connectedCallback();
-    window.addEventListener('click', (e:any) => {   
-      console.log(e);
-      console.log(this.shadowRoot)
-      if(this.shadowRoot)
-      console.log(this.shadowRoot.contains(e.target))
-      // if (document.getElementById('clickbox').contains(e.target)){
-      //   // Clicked in box
-      // } else{
-      //   // Clicked outside the box
-      // }
+    document.addEventListener('click', (event) => {
+      if (!event.composedPath().includes(this)) {
+        this._closeButton();
+      }
     });
+
+    const dropDownItems = document.querySelectorAll(".dropdown-item");
+    for (let item of dropDownItems) {
+      item.addEventListener("click", this._closeButton);
+    }
+  }
+
+
+  override disconnectedCallback() {
+    super.disconnectedCallback()
+    const dropDownItems = document.querySelectorAll(".dropdown-item");
+    for (let item of dropDownItems) {
+      item.removeEventListener("click", this._closeButton);
+    }
   }
 
   override render() {
-    return  html`
-                  <div @click="${this._handleClick}">
-                    <button  type="button"
+    return html`
+                  <div>
+                    <button type="button"
+                          @click="${this._handleClick}"
                           id=${this.id}
                           title=${this.label} 
                           class=${this.class}
@@ -193,13 +327,13 @@ export class DefDropDownBtn extends LitElement {
                      </button>
                   </div>
                 <div>
-                 <slot></slot>
+                  <slot></slot>
                 </div>
 
                
                `;
   }
-  
+
 
 }
 
